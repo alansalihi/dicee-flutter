ce65107503a0e1a2967cbf58b4f1ab32cd96f4a8
Alan Salihi <alansalihi@gmail.com>
2019-12-01 22:11:14 +0100

    complete v 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +16,59 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class Dicee extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container();
+  }
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int lDice = 1;
+  int rDice = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      lDice = Random().nextInt(6) + 1;
+      rDice = Random().nextInt(6) + 1;
+      print('Dices are: $lDice, $rDice');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  print('left button got pressed!');
+                  changeDiceFace();
+                },
+                child: Image.asset('images/dice$lDice.png'),
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  print('right button got pressed!');
+                  changeDiceFace();
+                },
+                child: Image(
+                  image: AssetImage('images/dice$rDice.png'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
